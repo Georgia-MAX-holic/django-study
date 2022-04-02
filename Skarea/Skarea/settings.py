5ef9d1ee057e5c50a02c2ef7f037c20fe440c6cc
Skarea <mekajone5@naver.com>
2022-04-02 19:59:30 +0900

    1

--- Skarea/Skarea/settings.py
+++ Skarea/Skarea/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$w$64uj!qel(ym&z%r(*b0tcgol^tfozg*e%7d&hf^4#7_!v9x'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
