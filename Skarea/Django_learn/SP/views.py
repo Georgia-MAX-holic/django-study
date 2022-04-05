dc51676e0f76d671fa3ec1c0231abbf744772394
Skarea <mekajone5@naver.com>
2022-04-05 21:53:22 +0900

    DB set

--- Skarea/Django_learn/SP/views.py
+++ Skarea/Django_learn/SP/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from SP.models import HelloWorld
 # Create your views here.
 
 def hello_world(request):
     if request.method =="POST":
+        temp= request.POST.get("hello_world_input")
         
-        return render(request, "SP/hello_world.html", context={"text": "POST METHOD!!"})
+        new_hello_world=HelloWorld()
+        new_hello_world.text =temp
+        new_hello_world.save()
+        
+        return render(request, "SP/hello_world.html", context={"text": new_hello_world  })
     else:
         return render(request, "SP/hello_world.html", context={"text": "GET METHOD!!"})
