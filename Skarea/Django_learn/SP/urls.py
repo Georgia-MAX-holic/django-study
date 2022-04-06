ec10e156c844db8aa33d7117b2c97bf7fbe174d2
Skarea <mekajone5@naver.com>
2022-04-06 20:13:17 +0900

    로그인 셋팅 , (리다이렉트 안됨)

--- Skarea/Django_learn/SP/urls.py
+++ Skarea/Django_learn/SP/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from SP.views import hello_world, AccountCreateView
+
+app_name ="SP"
+
 urlpatterns = [
-    path('hello_world/', views.hello_world),
+    path('hello_world/', hello_world, name="hello_world"),
+    path("create/", AccountCreateView.as_view(), name="create"),
 ]
--- Skarea/Django_learn/SP/views.py
+++ Skarea/Django_learn/SP/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from SP.models import HelloWorld
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def hello_world(request):
@@ -13,10 +16,14 @@ def hello_world(request):
         new_hello_world.text =temp
         new_hello_world.save()
         
-        hello_world_list = HelloWorld.objects.all()
-        
         return HttpResponseRedirect(reverse("SP:hello_world"))
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, "SP/hello_world.html", context={"hello_world_list": hello_world_list})
     
+    
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy("SP:hello_world")
+    template_name = "SP/create.html"    
